7d9a05de97f5652550ab10e011c6f942f620cbab
Pham Van Duc <ducpv2@domain.vnext.vn>
2018-12-04 16:15:39 +0700

    add reactstrap

--- resources/js/components/ProjectsList.js
+++ resources/js/components/ProjectsList.js
@@ -1,56 +1,78 @@
-    import axios from 'axios'
-    import React, { Component } from 'react'
-    import { Link } from 'react-router-dom'
+import axios from 'axios'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-    class ProjectsList extends Component {
-      constructor () {
-        super()
-        this.state = {
-          projects: []
-        }
-      }
+class ProjectsList extends Component {
 
-      componentDidMount () {
-        axios.get('/api/projects').then(response => {
-          this.setState({
-            projects: response.data
-          })
-        })
-      }
+  constructor() {
+    super()
+    this.state = {
+      projects: [],
+      modal: false
+    }
+    this.toggle = this.toggle.bind(this);
+  }
 
-      render () {
-        const { projects } = this.state
-        return (
-          <div className='container py-4'>
-            <div className='row justify-content-center'>
-              <div className='col-md-8'>
-                <div className='card'>
-                  <div className='card-header'>All projects</div>
-                  <div className='card-body'>
-                    <Link className='btn btn-primary btn-sm mb-3' to='/create'>
-                      Create new project
+  componentDidMount() {
+    axios.get('/api/projects').then(response => {
+      this.setState({
+        projects: response.data
+      })
+    })
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    const { projects } = this.state
+    return (
+      <div className='container py-4'>
+        <div className='row justify-content-center'>
+          <div className='col-md-8'>
+            <div className='card'>
+              <div className='card-header'>All projects</div>
+              <Button color="danger" onClick={this.toggle}>Modal</Button>
+              <div className='card-body'>
+                <Link className='btn btn-primary btn-sm mb-3' to='/create'>
+                  Create new project
                     </Link>
-                    <ul className='list-group list-group-flush'>
-                      {projects.map(project => (
-                        <Link
-                          className='list-group-item list-group-item-action d-flex justify-content-between align-items-center'
-                          to={`/${project.id}`}
-                          key={project.id}
-                        >
-                          {project.name}
-                          <span className='badge badge-primary badge-pill'>
-                            {project.tasks_count}
-                          </span>
-                        </Link>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
+                <ul className='list-group list-group-flush'>
+                  {projects.map(project => (
+                    <Link
+                      className='list-group-item list-group-item-action d-flex justify-content-between align-items-center'
+                      to={`/${project.id}`}
+                      key={project.id}
+                    >
+                      {project.name}
+                      <span className='badge badge-primary badge-pill'>
+                        {project.tasks_count}
+                      </span>
+                    </Link>
+                  ))}
+                </ul>
               </div>
             </div>
           </div>
-        )
-      }
-    }
+        </div>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
 
-    export default ProjectsList
+    )
+  }
+}
+
+export default ProjectsList
